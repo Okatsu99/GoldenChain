120d3d6adfabaf942a7010ffd854535eb49f31b5
thunderdaawn <jeanmichelc.gomez@gmail.com>
2020-05-27 15:16:07 +0800

    email formats for receipt & prescription

--- spring/src/main/java/com/remd/spring/repository/ClinicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.remd.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.remd.spring.bean.Clinic;
-
-public interface ClinicRepository extends JpaRepository<Clinic, Integer> {
-
-}
