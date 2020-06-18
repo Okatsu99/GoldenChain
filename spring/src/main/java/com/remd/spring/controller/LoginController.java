d99405ba0d882ed58066d044996eeedf852afff2
marcelo <marcelo@localhost>
2020-06-18 15:20:10 +0800

    Implemented Logging System
    Ability to log now added. Just have to add it to the controllers and
    other things.
    Prescriptions are now tied to the Patient(PatientRecord) in the DB.
    Emails with PatientRecord should now show up properly.

--- spring/src/main/java/com/remd/spring/controller/LoginController.java
+++ spring/src/main/java/com/remd/spring/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.remd.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 	@GetMapping("/login")
 	public String login() {
+		Logger logger = LoggerFactory.getLogger(LoginController.class.getName());
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
 		return "login";
 	}
 }
--- spring/src/main/java/com/remd/spring/controller/rest/AppointmentRestController.java
+++
@@ -1,8 +0,0 @@
-package com.remd.spring.controller.rest;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AppointmentRestController {
-
-}
